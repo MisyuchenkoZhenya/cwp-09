fc1363f59a37794a9437895547f2c533d235537c
Zhenya <misyuchenko.zhenya@mail.ru>
2017-11-10 01:02:23 +0300

    tasks 1, 2

---
+++ pokeapi.js
@@ -0,0 +1,39 @@
+const Promise = require('bluebird');
+const axios = require('axios');
+
+axios.get('http://pokeapi.co/api/v2/pokemon/42')
+  .then((response) => {
+    console.log(`    id: ${response.data.id}
+    Name: ${response.data.name}
+    Weight: ${response.data.weight}
+    Height: ${response.data.height}`);
+    })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+let poke_array = []
+for (var i = 0; i < 3; ++i) {
+  poke_array.push(
+    axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${i}0`)
+    .then((response) => { 
+      return response.data.results;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  );
+}
+
+Promise.all(poke_array)
+  .then((response) => {
+    response.forEach((arr, counter) => {
+        arr.forEach((pokemon, pok_number) => {
+            console.log(`${counter + 1}.${pok_number + 1}: ${pokemon.name}`);
+        });
+    });
+  })
+  .catch((error) => {
+      console.log(error);
+});
